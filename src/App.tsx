813e98a5dc07c0163317ced13601a522756a3ceb
James Jilhaney <notail2111@gmail.com>
2023-08-03 10:01:47 +0800

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import SignupPage from "./features/authorization/components/signup_page";
+import { Route, Routes } from "react-router-dom";
+import SignupPage from "./features/authorization/pages/unauthorized/signup_page";
+import HomePage from "./features/authorization/pages/authorized/home_page";
+import NotFoundPage from "./features/authorization/pages/404/404";
 
 function App() {
 
   return (
-    <SignupPage />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="signup" element={<SignupPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
