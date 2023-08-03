d084e068d74c4307ee49dc904be2c15f03866ed3
James Jilhaney <notail2111@gmail.com>
2023-08-03 20:49:15 +0800

    Created private route component

--- src/features/utils/components/private_routes.tsx
+++ src/features/utils/components/private_routes.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
 
 export default function PrivateRoutes() {
+  const auth = false;
+  return (
+    auth ? <Outlet /> : <Navigate to="login" />
+  );
 
 }
