a70d21d45571f9464ee1a891c4e929dba5a4280f
James Jilhaney <notail2111@gmail.com>
2023-08-03 20:49:23 +0800

    Added private route to routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import NotFoundPage from "./features/authentication/pages/404/404";
 import HomePage from "./features/authentication/pages/authorized/home_page";
 import LoginPage from "./features/authentication/pages/unauthorized/signup_page";
+import PrivateRoutes from "./features/utils/components/private_routes";
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
       <Route path="login" element={<LoginPage />} />
+      <Route element={<PrivateRoutes />}>
+        <Route path="/" element={<HomePage />} />
+      </Route>
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
